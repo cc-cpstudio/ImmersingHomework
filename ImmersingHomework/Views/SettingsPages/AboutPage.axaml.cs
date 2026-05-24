8e5226510b977ec87a7e96d88c82a885c057bf4e
programmer-cc <2140130341@qq.com>
2026-05-24 21:16:30 +0800

    feat 新增版本号，准备发版

--- ImmersingHomework/Views/SettingsPages/AboutPage.axaml.cs
+++ ImmersingHomework/Views/SettingsPages/AboutPage.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Serilog;
+using System.Reflection;
 
 namespace ImmersingHomework.Views.SettingsPages;
 
@@ -12,5 +13,25 @@ public partial class AboutPage : UserControl
     {
         _logger.Debug("AboutPage 初始化");
         InitializeComponent();
+        LoadVersion();
+    }
+
+    private void LoadVersion()
+    {
+        try
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version;
+            if (version != null)
+            {
+                VersionText.Text = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+            }
+            _logger.Debug("程序集版本已加载: {Version}", VersionText.Text);
+        }
+        catch (System.Exception ex)
+        {
+            _logger.Error(ex, "加载程序集版本失败");
+            VersionText.Text = "未知版本";
+        }
     }
 }
