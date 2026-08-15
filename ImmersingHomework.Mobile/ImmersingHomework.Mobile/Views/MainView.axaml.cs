e0502567f4f54ae0d582f22af6551e1817ce98e7
programmer_cc <2140130341@qq.com>
2026-08-15 12:03:49 +0800

    feat ImmersingHomework.Mobile

---
+++ ImmersingHomework.Mobile/ImmersingHomework.Mobile/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+
+using Avalonia;
+using Avalonia.Controls;
+
+namespace ImmersingHomework.Mobile.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ImmersingHomework.Mobile/ImmersingHomework.Mobile/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ImmersingHomework.Mobile.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
